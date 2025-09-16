abaee692a6b0db5971177bc11008038151f93a78
Hieudev <kieuphuonghoai@gmail.com>
2025-09-16 14:24:00 +0700

    integrate Spring Security for authentication

--- identity-service/target/generated-sources/annotations/com/devteria/identity_service/mapper/UserMapperImpl.java
+++ identity-service/target/generated-sources/annotations/com/devteria/identity_service/mapper/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-09-16T12:18:15+0700",
+    date = "2025-09-16T13:54:31+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.8 (Oracle Corporation)"
 )
 @Component
